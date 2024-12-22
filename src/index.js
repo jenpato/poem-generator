b5d7232decebbbd734ef1ea008c2aed7bb233450
jenpato <jenny.paterson1@gmail.com>
2024-12-23 08:32:13 +1100

    Updated context

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let apiKey = "1f608o64302d1t4618a7b9c696c97558";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let context =
-    "You are a poem expert and love to write short poems. Your mission is to generate a four line poem in basic HTML. Make sure to follow the user instructions. Sign by 'SheCodes AI' inside a <strong> element";
+    "You are a poem expert and love to write short poems. Your mission is to generate a four line poem in basic HTML. Make sure to follow the user instructions. Please don't add any markdown in the response. Sign by 'SheCodes AI' inside a <strong> element";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
